78352ba1e0fb703f8e6598389d3fbbd341d5fed9
IKRAM <ikramlechqer@gmail.com>
2025-01-31 15:32:01 +0100

    code updated

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // import build-in fs and http modules in Node.js
+
 var http = require('http');
 var fs = require('fs');
 
@@ -8,24 +9,19 @@ var host = 'localhost';
 var port = 3000;
 
 // create a server 
-// checks if the requested URL path is /  
-// if yes it will read the file index.html and send it back as a response
 // if the html file isn't available you will recieve the error loading the page 
 // else the html file will apear in the screen as response
-
 const server = http.createServer((request, response) => {
-  if (request.url === '/') {
-    fs.readFile('index.html', (error, data) => {
-      if (error) {
-        response.statusCode = 500;
-        response.end('Error loading the page');
-      } else {
-        response.statusCode = 200;
-        response.setHeader('Content-Type', 'text/html');
-        response.end(data);
-      }
-    });
-  }
+  fs.readFile('index.html', (error, data) => {
+    if (error) {
+      response.statusCode = 500;
+      response.end('Error loading the page');
+    } else {
+      response.statusCode = 200;
+      response.setHeader('Content-Type', 'text/html');
+      response.end(data);
+    }
+  });
 });
 
 server.listen(port, host, () => {
